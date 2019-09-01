e401eb399e50ef676cdde273cbd202f5fa95f0d8
tenpadrummer <4460drums@gmail.com>
2019-09-01 21:15:57 +0900

    create_group_function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,13 @@
 class MessagesController < ApplicationController
+  before_action :set_group
+
+  def index
+
+  end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
 end
